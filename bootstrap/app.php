ca070db186c857ff1bcd85eb8134b0a438820ba1
DrChic0 <DrChic0@github.com>
2025-05-17 15:40:05 -0700

    logout 3

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
-            'logout'
+            '/logout'
         ]);
     })->create();
