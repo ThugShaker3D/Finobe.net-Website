ce6841a611b068d61f2d0af5e85fd012fc82ef2a
DrChic0 <DrChic0@github.com>
2025-05-17 14:45:24 -0700

    fix

--- app/Http/Controllers/frontEnd.php
+++ app/Http/Controllers/frontEnd.php
@@ -148,7 +148,7 @@ class frontEnd extends Controller
                 return redirect('/auth/login');
             }
 
-            $user = (array) User::where('email', $data['email'])->first();
+            $user = User::where('email', $data['email'])->first()->toArray();
 
             if(!Hash::check($data['password'], $user['password'])) {
                 Session::put('errorlogin', true);
