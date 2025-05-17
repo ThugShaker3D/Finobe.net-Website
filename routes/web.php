8adc2d9ef363ec94e360441c78eb41e62a70c8be
DrChic0 <DrChic0@github.com>
2025-05-17 13:43:27 -0700

    ihiurgehuihuirge

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Middleware\SetClientIp;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware([SetClientIp::class])->group(function() {
-    Route::domain(str_replace('https://', '', env('APP_URL')))->group(function() {
+    Route::domain(str_replace('https://', '', 'sitetest1.finobe.net'/*env('APP_URL')for when release replace*/))->group(function() {
         Route::fallback(function() {
             return view('v2/404', []);
         });
