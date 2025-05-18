32187017bcf42886fb09f06ce8ca356d09500f7c
DrChic0 <DrChic0@github.com>
2025-05-17 23:22:23 -0700

    some fix

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::middleware([SetClientIp::class])->group(function() {
         Route::get('/forum/home', [frontEnd::class, 'forum_home']);
         Route::get('/forum/home/{section}', [frontEnd::class, 'forum_section']);
         Route::get('/forum/search', [frontEnd::class, 'forum_search']);
+        Route::match(['post', 'get'], '/forum/post', [frontEnd::class, 'forum_post']);
         Route::match(['post', 'get'], '/logout', [frontEnd::class, 'logout']);
 
         Route::prefix('api')->group(function() {
