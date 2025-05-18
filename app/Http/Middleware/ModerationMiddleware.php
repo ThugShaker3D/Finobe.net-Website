15db40bd4d0a87b4dd889a3c3d58dc4dccebc6a2
DrChic0 <DrChic0@github.com>
2025-05-18 16:07:12 -0700

    forgor

--- app/Http/Middleware/ModerationMiddleware.php
+++ app/Http/Middleware/ModerationMiddleware.php
@@ -6,6 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
 use Symfony\Component\HttpFoundation\Response;
 
 class ModerationMiddleware
