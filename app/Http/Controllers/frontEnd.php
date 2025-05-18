a807798a7948f4ecfe63e6f5713c1afb2b71ceda
DrChic0 <DrChic0@github.com>
2025-05-18 16:05:06 -0700

    2016

--- app/Http/Controllers/frontEnd.php
+++ app/Http/Controllers/frontEnd.php
@@ -131,61 +131,6 @@ class frontEnd extends Controller
                         'lastdiu' => DB::raw('DATE_ADD(CURRENT_TIMESTAMP(), INTERVAL 1 DAY)')
                     ]);
             }
-
-            if($this->request['data']['page'] != '/logout' && !$request->isMethod('post') && $this->request['data']['dir'] != '/email') {
-                if($this->request['data']['user']['verified'] == 'n' && $this->request['data']['page'] != '/legal/welcome') {
-                    return view($this->request['data']['user']['version'] . '/Verify', $this->request);
-                }
-            }
-
-            if($this->db->table('warning')->where('username', $this->request['data']['user']['username'])->where('reactivated', 'n')->exists()) {
-                $this->request['data']['isCurrentlyBanned'] = true;
-                $this->request['data']['moderationType'] = 1;
-                $this->request['data']['ban_info'] = (array) $this->db->table('warning')
-                    ->where('username', $this->request['data']['user']['username'])
-                    ->where('reactivated', 'n')
-                    ->first();
-                
-                return view($this->request['data']['user']['version'] . '/Moderation', $this->request);
-            }
-
-            if($this->db->table('bans')->where('username', $this->request['data']['user']['username'])->where('reactivated', 'n')->exists()) {
-                $this->request['data']['isCurrentlyBanned'] = $this->db->table('bans')->where('username', $this->request['data']['user']['username'])->where('reactivated', 'n')->where(DB::raw('TIMESTAMPDIFF(SECOND, NOW(), expire)'), '>', 0)->exists();
-                $this->request['data']['moderationType'] = 2;
-                $this->request['data']['ban_info'] = (array) $this->db->table('bans')
-                    ->where('username', $this->request['data']['user']['username'])
-                    ->where('reactivated', 'n')
-                    ->first();
-                
-                $currentDateTime = new \DateTime('now', new \DateTimeZone('America/Los_Angeles'));
-                $futureDateTime = new \DateTime($this->request['data']['ban_info']['expire'], new \DateTimeZone('America/Los_Angeles'));
-                    
-                $timeDifference = $currentDateTime->diff($futureDateTime);
-                    
-                $days = $timeDifference->d;
-                $hours = $timeDifference->h;
-                $minutes = $timeDifference->i;
-                $seconds = $timeDifference->s;
-
-                $format = '';
-                if ($days > 0) {
-                    $format .= $days . ' day' . ($days > 1 ? 's' : '');
-                }
-                if ($hours > 0) {
-                    $format .= ($format !== '' ? ', ' : '') . $hours . ' hour' . ($hours > 1 ? 's' : '');
-                }
-                if ($minutes > 0) {
-                    $format .= ($format !== '' ? ' and ' : '') . $minutes . ' minute' . ($minutes > 1 ? 's' : '');
-                }
-                if ($seconds > 0) {
-                    $format .= ($format !== '' ? ' and ' : '') . $seconds . ' second' . ($seconds > 1 ? 's' : '');
-                }
-
-                $this->request['data']['expiration'] = $format;
-                $this->request['data']['expiration_formatted'] = date('Y-m-d', strtotime($this->request['data']['ban_info']['expire']));
-                
-                return view($this->request['data']['user']['version'] . '/Moderation', $this->request);
-            }
         } else {
             $this->request['data']['embeds']['title'] .= 'Aesthetiful';
             $this->request['data']['embeds']['image'] .= 'logo.png';
--- app/Http/Middleware/ModerationMiddleware.php
+++ app/Http/Middleware/ModerationMiddleware.php
@@ -5,9 +5,12 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class ModerationMiddleware
 {
+    protected $db;
+    protected $request;
     /**
      * Handle an incoming request.
      *
@@ -15,6 +18,103 @@ class ModerationMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if(Auth::check()) {
+            $this->db = DB::connection('finobe');
+            $this->request = [
+                'data' => [
+                    'embeds' => [
+                        'title' => ' - ',
+                        'description' => 'Finobe, it is website. He is for old brick-builder. Good for use.',
+                        'url' => env('APP_URL'),
+                        'image' => env('APP_URL') . '/s/img/'
+                    ],
+                    'csrf_token' => View::share('csrf_token', csrf_token()),
+                    'siteusername' => true,
+                    'user' => [
+                        'version' => 'v2',
+                        'branding' => 'aesthetiful' // default branding
+                    ],
+                    'page' => strtok($_SERVER['REQUEST_URI'], '?'),
+                    'dir' => str_replace('\\', '', '/' . explode('/', trim($_SERVER['REQUEST_URI'], '/'))[0] . '/'),
+                    'lucky_number' => rand(0, User::count()) . '/' . User::count()
+                ]
+            ];
+
+            $this->request['data']['user'] = Auth::user()->toArray();
+            $this->request['data']['user']['formattedDius'] = $this->dataService->formatNumber($this->request['data']['user']['Dius']);
+            $this->request['data']['embeds']['title'] .= ($this->request['data']['user']['branding'] == 'finobe') ? 'Finobe' : 'Aesthetiful';
+            
+            if($this->request['data']['user']['branding'] == 'finobe') {
+                if($this->request['data']['user']['logo'] == 'v1') {
+                    $this->request['data']['embeds']['image'] .= 'BUSY.png';
+                } elseif($this->request['data']['user']['logo'] == 'v2') {
+                    $this->request['data']['embeds']['image'] .= 'finnobe3.png';
+                } else {
+                    $this->request['data']['embeds']['image'] .= 'finnobe3logo.png';
+                }
+            } else {
+                $this->request['data']['embeds']['image'] .= 'logo.png';
+            }
+
+            $this->request['data']['user']['friends'] = json_decode($this->request['data']['user']['friends'], true);
+            $this->request['data']['user']['avatar'] = json_decode($this->request['data']['user']['avatar'], true);
+
+            if($this->request['data']['page'] != '/logout' && !$request->isMethod('post') && $this->request['data']['dir'] != '/email') {
+                if($this->request['data']['user']['verified'] == 'n' && $this->request['data']['page'] != '/legal/welcome') {
+                    return view($this->request['data']['user']['version'] . '/Verify', $this->request);
+                }
+            }
+
+            if($this->db->table('warning')->where('username', $this->request['data']['user']['username'])->where('reactivated', 'n')->exists()) {
+                $this->request['data']['isCurrentlyBanned'] = true;
+                $this->request['data']['moderationType'] = 1;
+                $this->request['data']['ban_info'] = (array) $this->db->table('warning')
+                    ->where('username', $this->request['data']['user']['username'])
+                    ->where('reactivated', 'n')
+                    ->first();
+                
+                return view($this->request['data']['user']['version'] . '/Moderation', $this->request);
+            }
+
+            if($this->db->table('bans')->where('username', $this->request['data']['user']['username'])->where('reactivated', 'n')->exists()) {
+                $this->request['data']['isCurrentlyBanned'] = $this->db->table('bans')->where('username', $this->request['data']['user']['username'])->where('reactivated', 'n')->where(DB::raw('TIMESTAMPDIFF(SECOND, NOW(), expire)'), '>', 0)->exists();
+                $this->request['data']['moderationType'] = 2;
+                $this->request['data']['ban_info'] = (array) $this->db->table('bans')
+                    ->where('username', $this->request['data']['user']['username'])
+                    ->where('reactivated', 'n')
+                    ->first();
+                
+                $currentDateTime = new \DateTime('now', new \DateTimeZone('America/Los_Angeles'));
+                $futureDateTime = new \DateTime($this->request['data']['ban_info']['expire'], new \DateTimeZone('America/Los_Angeles'));
+                    
+                $timeDifference = $currentDateTime->diff($futureDateTime);
+                    
+                $days = $timeDifference->d;
+                $hours = $timeDifference->h;
+                $minutes = $timeDifference->i;
+                $seconds = $timeDifference->s;
+
+                $format = '';
+                if ($days > 0) {
+                    $format .= $days . ' day' . ($days > 1 ? 's' : '');
+                }
+                if ($hours > 0) {
+                    $format .= ($format !== '' ? ', ' : '') . $hours . ' hour' . ($hours > 1 ? 's' : '');
+                }
+                if ($minutes > 0) {
+                    $format .= ($format !== '' ? ' and ' : '') . $minutes . ' minute' . ($minutes > 1 ? 's' : '');
+                }
+                if ($seconds > 0) {
+                    $format .= ($format !== '' ? ' and ' : '') . $seconds . ' second' . ($seconds > 1 ? 's' : '');
+                }
+
+                $this->request['data']['expiration'] = $format;
+                $this->request['data']['expiration_formatted'] = date('Y-m-d', strtotime($this->request['data']['ban_info']['expire']));
+                
+                return view($this->request['data']['user']['version'] . '/Moderation', $this->request);
+            }
+        }
+        
         return $next($request);
     }
 }
