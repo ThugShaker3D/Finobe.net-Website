fde95b0e62272c18173a45e73df206b785b433f4
DrChic0 <DrChic0@github.com>
2025-05-18 21:41:02 -0700

    hrthrt

--- app/Http/Controllers/frontEnd.php
+++ app/Http/Controllers/frontEnd.php
@@ -577,7 +577,7 @@ class frontEnd extends Controller
                 'username' => $result['username'],
                 'pfp' => $result['pfp'],
                 'lastlogin' => date('m/d/Y h:i A', strtotime($result['lastlogin'])),
-                'IsOnline' => Carbon::parse($result['lastlogin'])->gt(Carbon::now()->subMinutes(2))
+                'IsOnline' => Carbon::parse($result['lastlogin'])->gt(Carbon::now()->addMinutes(2))
             ];
         }
 
