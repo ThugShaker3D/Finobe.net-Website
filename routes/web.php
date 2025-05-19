38dbbad65261e77162c35990b20d8d6ddd2a3c63
DrChic0 <DrChic0@github.com>
2025-05-18 19:36:50 -0700

    hjtrjrty

--- routes/web.php
+++ routes/web.php
@@ -31,11 +31,11 @@ Route::middleware([SetClientIp::class])->group(function() {
         });
 
         Route::prefix('forum')->group(function() {
-            Route::get('/edit', [frontEnd::class, 'forum_edit_reply']);
             Route::get('/home', [frontEnd::class, 'forum_home']);
             Route::get('/home/{section}', [frontEnd::class, 'forum_section']);
             Route::get('/search', [frontEnd::class, 'forum_search']);
             Route::match(['post', 'get'], '/post', [frontEnd::class, 'forum_post']);
+            Route::match(['post', 'get'], '/edit', [frontEnd::class, 'forum_edit_reply']);
             Route::match(['post', 'get'], '/new/reply', [frontEnd::class, 'forum_reply']);
         });
 
