1ee24aeea92cb67218f9718c91412f105c8b1bf7
DrChic0 <DrChic0@github.com>
2025-05-19 21:35:23 -0700

    ,jh,,u

--- app/Http/Controllers/api.php
+++ app/Http/Controllers/api.php
@@ -774,7 +774,7 @@ class api extends Controller
 
         if(!isset($results)) {
             $this->response['code'] = 400;
-            $this->response['message'] = 'Bad requestg';
+            $this->response['message'] = 'Bad request';
 
             return response()->json($this->response, 400);
         }
