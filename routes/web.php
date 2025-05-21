2d5f81385eb51ed70c6e6addb8ac544afc972ac7
DrChic0 <DrChic0@github.com>
2025-05-21 11:48:18 -0700

    reppore

--- routes/web.php
+++ routes/web.php
@@ -57,7 +57,7 @@ Route::middleware([SetClientIp::class])->group(function() {
         });
 
         Route::prefix('catalog')->group(function() {
-            Route::match(['post', 'get', 'options'], '/catalog/new', [frontEnd::class, 'catalog_new']);
+            Route::match(['post', 'get', 'options'], '/new', [frontEnd::class, 'catalog_new']);
             Route::get('/{section}', [frontEnd::class, 'catalog_index']);
 
             Route::get('/', function() {
