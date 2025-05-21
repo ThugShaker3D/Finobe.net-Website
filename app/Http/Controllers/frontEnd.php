1f46a3f6c111344e9b81a01c278cf854544d16a8
DrChic0 <DrChic0@github.com>
2025-05-20 20:11:33 -0700

    jyrjyr

--- app/Http/Controllers/frontEnd.php
+++ app/Http/Controllers/frontEnd.php
@@ -2489,12 +2489,12 @@ class frontEnd extends Controller
             return redirect('/password/reset');
         }
 
+        $resetid = hash_hmac('sha256', $id, 'privatekey');
+
         $id = $this->db->table('reset_password')->insertGetId([
             'username' => $user->username,
             'uid' => $resetid
         ]);
-
-        $resetid = hash_hmac('sha256', $id, 'privatekey');
 
         $html = file_get_contents(storage_path('reset_password_template.php'));
         $keywords = ['UUID', 'SIGNATURE', 'RESETID'];
