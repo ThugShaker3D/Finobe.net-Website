462278a50ccfe6c983a7b3f54ea58888b0082644
DrChic0 <DrChic0@github.com>
2025-05-22 09:59:34 -0700

    jjjjj

--- app/Http/Controllers/frontEnd.php
+++ app/Http/Controllers/frontEnd.php
@@ -148,7 +148,7 @@ class frontEnd extends Controller
 
         $this->request['data']['alerts']['announcements'] = $this->db->table('announcements')
             ->select('message', 'expire', 'author', 'color')
-            ->where('expire', '>', now())
+            ->where('expire', '>', DB::raw('NOW()'))
             ->orderBy('id', 'DESC')
             ->get()
             ->map(function ($item) {
