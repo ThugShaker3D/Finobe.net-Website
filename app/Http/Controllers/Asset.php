5735fa53201791dc17b99c63c2a6b265371d9568
DrChic0 <DrChic0@github.com>
2025-05-22 13:23:59 -0700

    facades

--- app/Http/Controllers/Asset.php
+++ app/Http/Controllers/Asset.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facade\DB;
-use Illuminate\Support\Facade\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class Asset extends Controller
 {
