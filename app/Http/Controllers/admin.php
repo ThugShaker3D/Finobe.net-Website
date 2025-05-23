ff3941ec86f37f7299a9a478e01c34f8b147dac6
DrChic0 <DrChic0@github.com>
2025-05-23 08:43:49 -0700

    skull emoji

--- app/Http/Controllers/admin.php
+++ app/Http/Controllers/admin.php
@@ -716,6 +716,7 @@ class admin extends Controller
                 $data['description'] ?? '',
                 intval($data['price']),
                 isset($data['onsale']),
+                false,
                 [],
                 ($data['type'] == 'hat' ? 8 : 19)
             );
