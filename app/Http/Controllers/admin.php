80906b66d30101e5841c507b60517e24c9b30381
DrChic0 <DrChic0@github.com>
2025-05-22 17:53:11 -0700

    I FORGOT HTTP

--- app/Http/Controllers/admin.php
+++ app/Http/Controllers/admin.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Validator;
 
