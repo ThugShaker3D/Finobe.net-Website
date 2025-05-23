88fee1b9b0b5ab3043d5cb67ade8df11158c1193
DrChic0 <DrChic0@github.com>
2025-05-23 07:49:31 -0700

    debugggg

--- app/Http/Controllers/api.php
+++ app/Http/Controllers/api.php
@@ -764,9 +764,9 @@ class api extends Controller
 
         $user = Auth::user();
 
-        if($user->status != 'admin' && Carbon::now()->diffInSeconds($user->render_cooldown) <= 30) {
+        if(Carbon::now()->diffInSeconds($user->render_cooldown) <= 30) {
             $this->response['code'] = 400;
-            $this->response['message'] = 'Whoa, cool down with the regeneration requests there!';
+            $this->response['message'] = 'Whoa, cool down with the regeneration requests there! (' . Carbon::now()->diffInSeconds($user->render_cooldown) . ')';
 
             return response()->json($this->response, 400);
         }
