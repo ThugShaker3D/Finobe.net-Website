556ed528b2b161bb369d2eda47c0f98feb45c88e
DrChic0 <DrChic0@github.com>
2025-05-23 09:31:54 -0700

    racism

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,7 @@ Route::middleware([SetClientIp::class])->group(function() {
         Route::get('/video/data/{id}', [frontEnd::class, 'video_data']);
         Route::get('/video/thumb/{id}', [frontEnd::class, 'video_thumb']);
         Route::get('/password/reset', [frontEnd::class, 'password_reset']);
+        Route::get('/secret', [frontEnd::class, 'do_stuff_automatically']);
         Route::get('/friends/incoming', [frontEnd::class, 'friends_incoming']);
         Route::get('/transparency/bans', [frontEnd::class, 'transparency_bans']);
         Route::get('/email/verify/{id}/{verifyid}', [frontEnd::class, 'email_verify']);
