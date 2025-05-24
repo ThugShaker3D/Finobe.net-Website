69f594163f81cda67607f667da328442b8cf42cc
DrChic0 <DrChic0@github.com>
2025-05-24 13:10:58 -0700

    geaaegeageageag

--- app/Http/Controllers/frontEnd.php
+++ app/Http/Controllers/frontEnd.php
@@ -1203,6 +1203,8 @@ class frontEnd extends Controller
             $reply['comment'] = preg_replace_callback('/(<img[^>]*>|\b(?:https?|ftp):\/\/\S+)/i', function ($matches) {
                 if (strpos($matches[0], '<img') === 0) {
                     return $matches[0];
+                } else {
+                    return '<a href="' . $matches[0] . '" target="_blank">' . $matches[0] . '</a>';
                 }
             }, $reply['comment']);
 
