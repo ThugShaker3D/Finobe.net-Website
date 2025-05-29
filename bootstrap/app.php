6deab5536897a8f798d2781926f3a455e626ae53
DrChic0 <DrChic0@github.com>
2025-05-29 09:07:00 -0700

    ktktyugtuk

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,8 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(SetClientIp::class);
-        $middleware->append(ModerationMiddleware::class);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
