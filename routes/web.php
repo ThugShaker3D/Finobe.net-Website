28772b1e37b2d3e2b67189a28717c129f736a0c0
DrChic0 <DrChic0@github.com>
2025-05-29 09:14:02 -0700

    brb

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Middleware\LimitReqestPerIp;
 use App\Http\Middleware\ModerationMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware([SetClientIp::class, LimitReqestPerIp::class, ModerationMiddleware::class])->group(function() {
+Route::middleware([SetClientIp::class, ModerationMiddleware::class])->group(function() {
     Route::domain(parse_url(env('APP_URL'), PHP_URL_HOST))->group(function() {
         Route::fallback(function() {
             return response()->view('v2/404', [
