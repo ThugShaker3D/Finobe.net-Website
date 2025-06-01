8dd528df64bd12fcc6360accb3ceb6b1d8666b27
DrChic0 <DrChic0@github.com>
2025-06-01 16:58:15 -0700

    fjyjf

--- app/Http/Controllers/frontEnd.php
+++ app/Http/Controllers/frontEnd.php
@@ -1130,7 +1130,7 @@ class frontEnd extends Controller
             if (strpos($matches[0], '<img') === 0) {
                 return $matches[0];
             } else {
-                return '<a href="' . $matches[0] . '" target="_blank">' . $matches[0] . '</a>';
+                return '<a href="' . strip_tags($matches[0]) . '" target="_blank">' . $matches[0] . '</a>';
             }
         }, $post['comment']);
 
@@ -1188,7 +1188,7 @@ class frontEnd extends Controller
                 if (strpos($matches[0], '<img') === 0) {
                     return $matches[0];
                 } else {
-                    return '<a href="' . $matches[0] . '" target="_blank">' . $matches[0] . '</a>';
+                    return '<a href="' . strip_tags($matches[0]) . '" target="_blank">' . $matches[0] . '</a>';
                 }
             }, $sticked['comment']);
 
