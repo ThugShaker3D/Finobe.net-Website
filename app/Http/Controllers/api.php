2651e0389594f5bb5e3ffad2fe6a83ce13948d75
DrChic0 <DrChic0@github.com>
2025-06-06 08:03:05 -0700

    kjjjkjkkjjk

--- app/Http/Controllers/api.php
+++ app/Http/Controllers/api.php
@@ -95,7 +95,8 @@ class api extends Controller
         
         $this->response['data']['items']['pagination'] = [
             'current_page' => $currentPage,
-            'number_of_pages' => ceil(count($this->response['data']['items']['data'] ?? []) / $itemsPerPage)
+            'number_of_pages' => ceil(count($this->response['data']['items']['data'] ?? []) / $itemsPerPage),
+            'number_of_bruh' => count($this->response['data']['items']['data'] ?? [])
         ];
 
         return response()->json($this->response, 200);
