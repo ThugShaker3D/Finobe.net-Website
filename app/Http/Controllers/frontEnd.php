e9d680b4844c578638aa31e31a8db94184615abe
DrChic0 <DrChic0@github.com>
2025-06-08 14:47:57 -0700

    kuykuy

--- app/Http/Controllers/frontEnd.php
+++ app/Http/Controllers/frontEnd.php
@@ -2972,7 +2972,7 @@ class frontEnd extends Controller
                 $thumbnail = $filename . '.jpg';
                 $filename .= '.mp4';
 
-                $file = $request->file('file')->store('private/videos');
+                $file = $request->file('file')->store('videos');
                 $ffmpeg = FFmpeg::create();
                 $video = $ffmpeg->open(storage_path('app' . $file));
                 $format = new X264('aac', 'libx264');
