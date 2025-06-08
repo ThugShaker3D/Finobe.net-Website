662fa1ca77aaaa20f51f420c9562c861524a5931
DrChic0 <DrChic0@github.com>
2025-06-08 15:20:19 -0700

    hrehret

--- app/Http/Controllers/frontEnd.php
+++ app/Http/Controllers/frontEnd.php
@@ -2986,7 +2986,7 @@ class frontEnd extends Controller
                 */
 
                 ProcessVideo::dispatch($file, $filename, $thumbnail);
-                exec('php artisan queue:work redis &');
+                exec('nohup php artisan queue:work redis --sleep=3 --tries=3 > /dev/null 2>&1 &');
                 
                 $id = $this->db->table('videos')->insertGetId([
                     'title' => $data['title'],
