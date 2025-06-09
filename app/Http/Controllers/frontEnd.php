1e2b1e581dc4b7e9d2706d47ac81dd82fa657083
DrChic0 <DrChic0@github.com>
2025-06-08 18:45:23 -0700

    hrehre

--- app/Http/Controllers/frontEnd.php
+++ app/Http/Controllers/frontEnd.php
@@ -2987,7 +2987,7 @@ class frontEnd extends Controller
                 */
 
                 ProcessVideo::dispatch($file, $filename, $thumbnail);
-                exec('cd /var/www/Finobe && php artisan queue:work --timeout=21600 --sleep=3 --tries=3 > /dev/null 2>&1 &');
+                exec('cd /var/www/Finobe && php artisan queue:work --timeout=21600 > /dev/null 2>&1 &');
                 
                 $id = $this->db->table('videos')->insertGetId([
                     'title' => $data['title'],
