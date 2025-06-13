387a3cfdf63848afb01fb0a00dcadc20bf5dd277
iinotaku <iinotaku@pizdec.xyz>
2025-06-13 06:05:07 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -185,9 +185,7 @@ Route::middleware([SetClientIp::class, LimitRequestPerIp::class, ModerationMiddl
             });
             Route::get('/api/gameserver/register/{jobId}', [rbxAPIs::class, 'registerJobId']);
             Route::any('/api/gameserver/sysstats-report', function (Request $request) {
-                $a = new dataController();
-                $a->send_discord_message($request->query('Message'));
-                return "shit"
+                return response("no way", 500)
             });
             Route::get('/api/gameserver/visit/{jobId}', [rbxAPIs::class, 'visitJobId']);
             Route::get('/api/gameserver/shutdown/{jobId}', [rbxAPIs::class, 'shutdownJobId']);
