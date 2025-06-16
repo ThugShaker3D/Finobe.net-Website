fb4b08d5191693a20455865e2c22a2240b512eb8
DrChic0 <DrChic0@github.com>
2025-06-15 18:26:57 -0700

    bruh

--- app/Http/Controllers/api.php
+++ app/Http/Controllers/api.php
@@ -1121,7 +1121,7 @@ class api extends Controller
             $html = '
                 <div data-v-5ad0ed22="" title="' . htmlspecialchars($result['title']) . '" class="game-card-div">
                     <a data-v-5ad0ed22="" href="/place/' . $result['id'] . '" class="game-card-link">
-                        <span data-v-5ad0ed22="" class="game-card d-flex flex-column">
+                        <span data-v-5ad0ed22="" class="game-card d-flex flex-column" style="background-color: #35383c;">
                             ' . ($result['additional']['version'] == "2016" ? '<span data-v-5ad0ed22="" class="badge badge-danger position-absolute">2016</span>' : '') . '
                             <span data-v-5ad0ed22="" class="thumbnail">
                                 <div data-v-5ad0ed22="" class="vue-load-image"><img data-v-5ad0ed22="" src="https://cdn.finobe.net/' . $result['thumbnail'] . '" class="card-img-top"></div>
