d0c9d22b5947b5b1dc16e6e6b4a1ea9b4b5d2ec4
DrChic0 <DrChic0@github.com>
2025-06-17 13:09:21 -0700

    grejgioer

--- app/Http/Controllers/rbxAPIs.php
+++ app/Http/Controllers/rbxAPIs.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 
 class rbxAPIs extends Controller
 {
