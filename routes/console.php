614977d6500c138ab843e5d839871b6614af3d39
DrChic0 <DrChic0@github.com>
2025-07-10 20:47:37 -0700

    hreherrthy

--- routes/console.php
+++ routes/console.php
@@ -137,7 +137,7 @@ Artisan::command('something', function () {
     */
 
     $files = File::files('/var/www/cdn.finobe.net/avatar');
-    $avatars = User::pluck('pfp')->values()->toArray();
+    $avatars = User::pluck('pfp')->toArray();
 
     foreach($avatars as $key => $avatar) {
         continue;
