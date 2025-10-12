3a4695971dbe8171017bfe1a652087952f7e0fc1
unhappyworld <iinotaku@pizdec.xyz>
2025-10-12 20:53:39 +0300

    LuaWebService is here!

---
+++ app/Http/Controllers/Client/LuaWebServiceController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class LuaWebServiceController extends Controller
+{
+    public function handleSocialRequest (Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+			'method' => ['required'],
+            'groupid' => ['required_if:method,IsInGroup'],
+            'playerid' => ['required'],
+            'userid' => ['required_if:method,IsFriendsWith,IsBestFriendsWith'],
+		]);
+
+        if($validator->fails()) {
+            return response()->json(['code' => 0, 'error' => 'Bad Request'], status: 403);
+		}
+
+		$valid = $validator->valid();
+        switch ($valid['method']) {
+            case 'IsInGroup':
+                if ($valid['groupid'] == '1200769') {
+                    $user = User::where('id', $valid['playerid'])->first();
+
+                    return response('<Value Type="boolean">'.$user->status == 'admin'.'</Value>');
+                }
+        }
+
+        return response()->json(['code' => 0, 'error' => 'Bad Request'], status: 403);
+    }
+}
