b2bd5551d5d81f90e73259a55462bb2e506671a8
unhappyworld <iinotaku@pizdec.xyz>
2025-10-12 21:02:56 +0300

    Fix asset routes

--- routes/services/assetgame.php
+++ routes/services/assetgame.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\rbxAPIs;
 
 Route::group(['as' => 'asset-game.'], function(){
     Route::get('/asset', [rbxAPIs::class, 'asset'])->name('asset');
+    Route::get('/Asset', [rbxAPIs::class, 'asset']);
     Route::group(['prefix' => 'asset'], function() {
         Route::get('/CharacterFetch.ashx', [CharacterAppearanceController::class, 'characterFetch'])->name('character-fetch');
         Route::get('/BodyColors.ashx', [CharacterAppearanceController::class, 'bodyColors'])->name('body-colors');
