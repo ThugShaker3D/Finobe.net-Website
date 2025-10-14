d158d42d208d8481f0f25a2726ec84dc8ffef00c
unhappyworld <iinotaku@pizdec.xyz>
2025-10-14 16:06:36 +0300

    Add missing files

---
+++ app/Http/Controllers/Client/StudioJoinController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
+class StudioJoinController extends Controller
+{
+    public function visit(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+			'UserID' => ['required'],
+            'PlaceID'=> ['required'],
+		]);
+
+        if($validator->fails()) {
+            return response()->json(['code' => 0, 'error' => 'Bad Request'], status: 403);
+		}
+        $valid = $validator->valid();
+
+        $visitScript = Storage::get('penelope/scripts/visit.lua');
+        $visitScript = str_replace("{placeId}", $valid['PlaceID'], $visitScript);
+        $visitScript = str_replace("{userId}", $valid['UserID'], $visitScript);
+        return response();
+    }
+}
